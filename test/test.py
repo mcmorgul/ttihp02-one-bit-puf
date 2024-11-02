913e570dba379e08c2f44e1c7ea82a7c8a2b03ea
M Ceylan Morgul <49791617+mcmorgul@users.noreply.github.com>
2024-11-02 14:56:22 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -20,7 +20,8 @@ async def test_project(dut):
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
-
+    
+    await ClockCycles(dut.clk, 10)
     assert dut.uo_out.value == 1
 
     
